d50f6348a9cfc94179741342f879763968bf0e7c
shiyanfei5 <13218016202@163.com>
2019-09-09 23:29:46 +0800

    (*)value注解

--- learnspringboot/src/main/java/com/yfshi/class014/CDPlayer.java
+++ learnspringboot/src/main/java/com/yfshi/class014/CDPlayer.java
@@ -1,7 +1,11 @@
 package com.yfshi.class014;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Component("CDPlayer")
@@ -9,18 +13,33 @@ public class CDPlayer {
 
     CompactDisc smgPeppers;
 
+    List<String> li;
+
 
     public CDPlayer(CompactDisc smgPeppers){
         System.out.println("CDPlayer 进行了CDPlayer(CompactDisc smgPeppers)初始化注入");
     }
+
     @Autowired
     public CDPlayer(CompactDisc smgPeppers,CompactDisc sgtPeppers){
         this.smgPeppers = sgtPeppers;
         System.out.println("CDPlayer 进行了CDPlayer(CompactDisc smgPeppers,CompactDisc sgtPeppers)初始化注入");
     }
 
+
+
+    public void valueTest(String s){
+        System.out.println("valueTest方法被调用");
+        List<String> tmp = new ArrayList<String>();
+        tmp.add(s);
+        this.li = tmp;
+    }
+
     public void start(){
         this.smgPeppers.player();
     }
+
+
+
 
 }
